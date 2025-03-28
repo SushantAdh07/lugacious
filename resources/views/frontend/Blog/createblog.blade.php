6d525c46ebdb6dea24e3b7934866d722aad96cc3
sushantadh07 <sushantadhikari2828@gmail.com>
2025-03-28 23:53:36 +0545

    blog again

--- resources/views/frontend/Blog/createblog.blade.php
+++ resources/views/frontend/Blog/createblog.blade.php
@@ -1,7 +1,17 @@
 @extends('frontend.home')
 @section('hero')
-    <label for="">Blog title</label>
-    <input type="text" name="blog_title" placeholder="Title">
-    <label for="">Blog Description</label>
-    <input type="text" name="blog_desc" placeholder="Description">
+    <div class="block p-4">
+        <form action="" method="POST">
+            @csrf
+            <div class="m-3">
+                <label for="">Blog title</label>
+                <input type="text" name="blog_title" placeholder="Title">
+            </div>
+            <div class="m-3">
+                <label for="">Blog Description</label>
+                <input type="text" name="blog_desc" placeholder="Description">
+            </div>
+            <a class="bg-black text-white p-2" href="">Save</a>
+        </form>
+    </div>
 @endsection
