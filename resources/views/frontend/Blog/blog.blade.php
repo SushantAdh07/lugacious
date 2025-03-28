a3fa378ea131c8e5b654c4381a6fa42e972bf5ba
sushantadh07 <sushantadhikari2828@gmail.com>
2025-03-28 23:43:56 +0545

    blog changes

--- resources/views/frontend/Blog/blog.blade.php
+++ resources/views/frontend/Blog/blog.blade.php
@@ -1,8 +1,13 @@
 @extends('frontend.home')
 @section('hero')
-    <h1>Blog Content</h1>
-    <h2>Top Nepalese Fashion Brands</h2>
-    <p>I was roaming around Internet sometimes Instagram, sometimes binge surfing I came across couple of Nepalese brand who
-        sells good and authentic product.</p>
-    <img src="" alt="">
-@endsection
+    @auth
+        @if (Auth::user()->role === 'admin')
+            <button>Create Blog</button @endif
+        @endauth
+        <h1>Blog Content</h1>
+        <h2>Top Nepalese Fashion Brands</h2>
+        <p>I was roaming around Internet sometimes Instagram, sometimes binge surfing I came across couple of Nepalese brand
+            who
+            sells good and authentic product.</p>
+        <img src="" alt="">
+    @endsection
