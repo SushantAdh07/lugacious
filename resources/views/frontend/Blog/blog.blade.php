4bacc1c243063c4a3c136668b2ecb12011ed0b70
sushantadh07 <sushantadhikari2828@gmail.com>
2025-03-28 23:48:14 +0545

    frontend

--- resources/views/frontend/Blog/blog.blade.php
+++ resources/views/frontend/Blog/blog.blade.php
@@ -1,13 +1,20 @@
 @extends('frontend.home')
 @section('hero')
-    @auth
-        @if (Auth::user()->role === 'admin')
-            <button>Create Blog</button @endif
-        @endauth
-        <h1>Blog Content</h1>
+    <div class="block mt-3 p-4">
+        <div>
+            @auth
+                @if (Auth::user()->role === 'admin')
+                    <a href="{{ route('create-blog') }}"
+                        class="bg-[#BF8E43] font-bold rounded-lg text-white px-6 py-3 text-sm">Create Blog</a>
+                @endif
+            @endauth
+        </div>
+        <h1 class="mt-2">Blog Content</h1>
         <h2>Top Nepalese Fashion Brands</h2>
-        <p>I was roaming around Internet sometimes Instagram, sometimes binge surfing I came across couple of Nepalese brand
+        <p>I was roaming around Internet sometimes Instagram, sometimes binge surfing I came across couple of Nepalese
+            brand
             who
             sells good and authentic product.</p>
         <img src="" alt="">
-    @endsection
+    </div>
+@endsection
