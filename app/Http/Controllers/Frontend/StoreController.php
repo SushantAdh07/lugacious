66151988e1cae47a72404195acee789f77dfa77c
sushantadh07 <sushantadhikari2828@gmail.com>
2025-04-05 23:43:19 +0545

    Repo -2

--- app/Http/Controllers/Frontend/StoreController.php
+++ app/Http/Controllers/Frontend/StoreController.php
@@ -5,36 +5,39 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreRequest;
 use App\Models\Store;
+use App\Repositories\Interfaces\StoreRepositoryInterface;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 
 class StoreController extends Controller
 {
+    protected $storeRepository;
+    public function __construct(StoreRepositoryInterface $storeRepository)
+    {
+        $this->storeRepository = $storeRepository;
+    }
+
     public function newStore(){
         return view('frontend.store.createstore');
     }
     
     public function storeDetails($id){
-        $store = Store::findOrFail($id);
+        $store = $this->storeRepository->all();
         return view('frontend.store.storedetails', compact('store'));
     }
 
-    public function createStore(Request $request)
+    public function createStore(StoreRequest $request)
 {
-        $imagePath = $request->file('store_image')->store('images', 'public');
-        
+        $data = $request->validated();
 
-        Store::insert([
-            'user_id'=>Auth::id(),
-            'store_name'=> $request->store_name,
-            'store_description'=> $request->store_description,
-            'store_category'=>$request->store_category,
-            'store_image'=>$imagePath,
-            'store_followers'=>$request->store_followers,
-            'store_insta'=>$request->store_insta,
-            
-        ]);
+        if ($request->hasFile('store_image')){
+            $imagePath = $request->file('store_image')->store('images', 'public');
+            $data['store_image'] = $imagePath;
+        }
+
+        $this->storeRepository->create($data);
+        
         return redirect()->route('home');
 }
 
--- app/Repositories/StoreRepository.php
+++ app/Repositories/StoreRepository.php
@@ -2,6 +2,34 @@
 
 namespace App\Repositories;
 
-class StoreRepository{
-    
+use App\Models\Store;
+use App\Repositories\Interfaces\StoreRepositoryInterface;
+
+class StoreRepository implements StoreRepositoryInterface{
+    protected $model;
+
+    public function __construct(Store $store)
+    {
+        $this->model = $store;
+    }
+
+    public function all(){
+        return $this->model->latest()->get();
+    }
+
+    public function find($id){
+        //
+    }
+
+    public function create($data){
+        return $this->model->create($data);
+    }
+
+    public function update($id, $data){
+        //
+    }
+
+    public function delete($id){
+        //
+    }
 }
