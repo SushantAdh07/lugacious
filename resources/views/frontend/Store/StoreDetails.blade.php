5ca61fa128bbd55098018502ccf8bb9b435dc2d2
sushantadh07 <sushantadhikari2828@gmail.com>
2025-04-09 23:01:32 +0545

    frontend

--- resources/views/frontend/Store/StoreDetails.blade.php
+++ resources/views/frontend/Store/StoreDetails.blade.php
@@ -27,6 +27,24 @@
                 <p class="text-gray-600"><span class="font-semibold">Category:</span> {{ $store->store_category }}</p>
                 <p class="text-gray-600"><span class="font-semibold">Instagram Followers:</span>
                     {{ $store->store_followers }}</p>
+                <p>Recent Items</p>
+
+
+                <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+                    <div>
+                        <img class="h-auto max-w-full rounded-lg"
+                            src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" alt="">
+                    </div>
+                    <div style="max-width: 400px; max-height: 600px; overflow: auto;">
+                        <blockquote class="instagram-media" data-instgrm-permalink="https://www.instagram.com/leomessi"
+                            data-instgrm-version="14">
+                            <!-- Instagram will load the feed here -->
+                        </blockquote>
+                        <script async src="//www.instagram.com/embed.js"></script>
+                    </div>
+
+                </div>
+
                 <button
                     class="bg-[#BF8e43] font-bold rounded-lg text-white px-6 py-3 text-sm hover:bg-white hover:text-[#BF8e43] hover:border-[#1915014a] border">Add
                     to
