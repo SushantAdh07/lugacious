5b4497661c40ecbe886ff77db9d354e674d9c0fb
Sushantadh07 <sushantadhikari2828@gmail.com>
2025-04-25 22:43:16 +0545

    intended route changed to home

--- resources/views/frontend/Body/header.blade.php
+++ resources/views/frontend/Body/header.blade.php
@@ -57,7 +57,7 @@
                         @else
                             <li>
                                 <a href="{{ route('login') }}"
-                                    class="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent transition-colors duration-200">
+                                    class="block py-2 px-3 text-gray-900 rounded-sm hover:text-[#BF8e43] md:hover:bg-transparent md:border-0 md:hover:text-[#BF8e43] md:p-0 dark:text-white md:dark:hover:text-[#BF8e43] dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent transition-colors duration-200 {{ request()->routeIs('login') ? 'md:text-[#BF8e43] md:dark:text-blue-500' : '' }}">
                                     Log In
                                 </a>
                             </li>
